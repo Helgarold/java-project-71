0bf85c9e5f3dec61158b6898106c7943f1911722
Helgarold <mipakk95@gmail.com>
2024-12-08 14:23:18 +0300

    Implemented input of arguments and options for the CLI utility

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -6,6 +6,14 @@ import picocli.CommandLine.Option;
 
 @Command(name = "gendiff", description = "Compares two configuration files and shows a difference.")
 public class App implements Runnable {
+    @Option(names = {"-f", "--format"}, description = "output format [default: stylish]", defaultValue = "stylish")
+    private String format;
+
+    @CommandLine.Parameters(index = "0", description = "path to first file")
+    private String filepath1;
+
+    @CommandLine.Parameters(index = "1", description = "path to second file")
+    private String filepath2;
 
     @Option(names = {"-h", "--help"}, usageHelp = true, description = "Show this help message and exit.")
     private boolean help;
@@ -19,9 +27,7 @@ public class App implements Runnable {
 
     @Override
     public void run() {
-        if (version) {
-            System.out.println("Version 1.0");
-        }
-        // Здесь можно будет добавить дальнейшую логику
+        // Здесь будет логика для сравнения и вывода результата, пока она не реализована
+        System.out.printf("Comparing %s and %s with format %s%n", filepath1, filepath2, format);
     }
 }
