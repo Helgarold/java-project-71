3f67204a3430e6af8c9fc2bd00ae174d1e2095a8
Helgarold <mipakk95@gmail.com>
2024-12-11 14:46:14 +0300

    Refactor App.java for improvements

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -10,19 +10,30 @@ import java.io.IOException;
 @Command(name = "gendiff", description = "Compares two configuration files and shows a difference.")
 public class App implements Runnable {
 
-    @Option(names = {"-f", "--format"}, description = "output format [default: stylish]", paramLabel = "format", defaultValue = "stylish")
+    @Option(names = {"-f", "--format"},
+            description = "output format [default: stylish]",
+            paramLabel = "format",
+            defaultValue = "stylish")
     private String format;
 
-    @Parameters(index = "0", description = "path to first file", paramLabel = "filepath1")
+    @Parameters(index = "0",
+            description = "path to first file",
+            paramLabel = "filepath1")
     private String filepath1;
 
-    @Parameters(index = "1", description = "path to second file", paramLabel = "filepath2")
+    @Parameters(index = "1",
+            description = "path to second file",
+            paramLabel = "filepath2")
     private String filepath2;
 
-    @Option(names = {"-h", "--help"}, usageHelp = true, description = "Show this help message and exit.")
+    @Option(names = {"-h", "--help"},
+            usageHelp = true,
+            description = "Show this help message and exit.")
     private boolean help;
 
-    @Option(names = {"-V", "--version"}, versionHelp = true, description = "Print version information and exit.")
+    @Option(names = {"-V", "--version"},
+            versionHelp = true,
+            description = "Print version information and exit.")
     private boolean version;
 
     public static void main(String[] args) {
