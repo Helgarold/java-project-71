2e4189842bd064334d2a58af863ef38f300bd047
Helgarold <mipakk95@gmail.com>
2024-12-15 14:01:50 +0300

    Fix import issues in YmlDifferTest

--- app/src/test/java/hexlet/code/YmlDifferTest.java
+++ app/src/test/java/hexlet/code/YmlDifferTest.java
@@ -1,11 +1,10 @@
 package hexlet.code;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 
 public class YmlDifferTest {
 
