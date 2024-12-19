645d5a49916f2097cc804e9d049e82610b448f7b
Helgarold <mipakk95@gmail.com>
2024-12-19 19:47:28 +0300

    Merge FileParser and Parser classes

--- app/src/main/java/hexlet/code/Parser.java
+++ app/src/main/java/hexlet/code/Parser.java
@@ -1,5 +1,8 @@
 package hexlet.code;
 
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.yaml.snakeyaml.Yaml;
 
 import java.io.FileInputStream;
@@ -10,23 +13,51 @@ import java.nio.file.Paths;
 import java.util.Map;
 
 public class Parser {
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+    private static final Yaml YAML = new Yaml();
 
-    // Метод для чтения YAML-файла и преобразования его в Map
+    // Метод для чтения файла и преобразования его в Map
     public static Map<String, Object> getData(String filePath) throws IOException {
-        // Проверка существования файла
         if (!Files.exists(Paths.get(filePath))) {
             throw new IOException("File not found: " + filePath);
         }
 
-        // Определение типа файла по расширению
         String fileExtension = getFileExtension(filePath);
-        if (!"yml".equalsIgnoreCase(fileExtension) && !"yaml".equalsIgnoreCase(fileExtension)) {
-            throw new IOException("Unsupported file type: " + fileExtension);
+        switch (fileExtension.toLowerCase()) {
+            case "json":
+                return getDataFromJsonFile(filePath);
+            case "yml":
+            case "yaml":
+                return getDataFromYamlFile(filePath);
+            default:
+                throw new IOException("Unsupported file type: " + fileExtension);
         }
+    }
 
+    // Метод для чтения JSON-файла и преобразования его в Map
+    private static Map<String, Object> getDataFromJsonFile(String filePath) throws IOException {
+        String content = new String(Files.readAllBytes(Paths.get(filePath)));
+        return parseJson(content);
+    }
+
+    // Метод для парсинга JSON
+    private static Map<String, Object> parseJson(String content) throws IOException {
+        try {
+            return OBJECT_MAPPER.readValue(content, Map.class);
+        } catch (JsonParseException e) {
+            throw new IOException("Invalid JSON format in file: " + content, e);
+        } catch (JsonMappingException e) {
+            throw new IOException("Error mapping JSON to object in file: " + content, e);
+        }
+    }
+
+    private static Map<String, Object> getDataFromYamlFile(String filePath) throws IOException {
         try (InputStream inputStream = new FileInputStream(filePath)) {
-            Yaml yaml = new Yaml();
-            return yaml.load(inputStream); // Парсинг содержимого
+            Map<String, Object> data = YAML.load(inputStream);
+            if (data == null) {
+                throw new IOException("YAML file contains invalid data: " + filePath);
+            }
+            return data; // Возврат разобранного содержимого
         } catch (IOException e) {
             throw new IOException("Error reading file: " + filePath, e);
         }
@@ -34,8 +65,7 @@ public class Parser {
 
     // Новый метод для загрузки данных из строки YAML
     public static Map<String, Object> getDataFromString(String ymlString) {
-        Yaml yaml = new Yaml();
-        return yaml.load(ymlString); // Парсинг содержимого из строки
+        return YAML.load(ymlString); // Парсинг содержимого из строки
     }
 
     // Вспомогательный метод для получения расширения файла
