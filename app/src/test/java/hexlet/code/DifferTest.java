27a62ca19d1e9decaf33b9ada614aa0d3b065dca
Helgarold <mipakk95@gmail.com>
2025-01-08 15:24:58 +0300

    Updated imports in the DifferTest class

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -1,11 +1,11 @@
 package hexlet.code;
 
 import org.junit.jupiter.api.Test;
-
 import java.io.IOException;
 import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DifferTest {
 
